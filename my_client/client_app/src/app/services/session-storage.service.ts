5dfa14b0cdfb5bcc11e51e6bd225828980922146
ngochien19 <ngochien191002@gmail.com>
2023-04-04 10:45:08 +0700

    hiền

--- my_client/client_app/src/app/services/session-storage.service.ts
+++ my_client/client_app/src/app/services/session-storage.service.ts
@@ -1,4 +1,3 @@
-// lưu trữ thông tin người dùng
 import { Injectable } from '@angular/core';
 import { IUser } from '../interfaces/user';
 
@@ -11,27 +10,27 @@ export class SessionStorageService {
   private readonly currentUserKey = 'currentUser';
 
   constructor() {}
-
+  // lấy danh sách user đã lưu trữ
   getUsers(): IUser[] {
   const usersString = sessionStorage.getItem(this.usersKey);
   return usersString ? JSON.parse(usersString) : [];
   }
-
+  // thêm mới user
   addUser(user: IUser) {
   const users = this.getUsers();
   users.push(user);
   sessionStorage.setItem(this.usersKey, JSON.stringify(users));
   }
-
+  // lấy người dùng hiện tại
   getCurrentUser(): IUser {
   const userString = sessionStorage.getItem(this.currentUserKey);
   return userString ? JSON.parse(userString) : null;
   }
-
+  // lưu user hiện tại
   setCurrentUser(user: IUser) {
   sessionStorage.setItem(this.currentUserKey, JSON.stringify(user));
   }
-
+  // xóa user hiện tại
   removeCurrentUser() {
   sessionStorage.removeItem(this.currentUserKey);
   }
