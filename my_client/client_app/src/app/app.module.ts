dd08e0db5f11f4c51559926f6c3a657f9dbbc583
khanhyencutephomaique <yennguyen220902@gmail.com>
2023-04-07 00:43:08 +0700

    yên update account page

--- my_client/client_app/src/app/app.module.ts
+++ my_client/client_app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
+
 import { AppComponent } from './app.component';
 import { AccountpageComponent } from './accountpage/accountpage.component';
 import { LoginComponent } from './login/login.component';
