9b5b01b2234c0a20c4b6964446e88aa5325a7899
khanhyencutephomaique <yennguyen220902@gmail.com>
2023-04-06 17:38:42 +0700

    yên update account page

--- my_client/client_app/src/app/app-routing.module.ts
+++ my_client/client_app/src/app/app-routing.module.ts
@@ -5,13 +5,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AccountpageComponent } from './accountpage/accountpage.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { AuthGuard } from './auth-guard';
+// import { AuthGuard } from './auth-guard';
 const routes: Routes = [
   {path: 'account', component:AccountpageComponent},
 
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // { path: '', redirectTo: '/home', pathMatch: 'full' },
   // { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'home', component: AccountpageComponent, canActivate: [AuthGuard] },
+  // { path: 'home', component: AccountpageComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
