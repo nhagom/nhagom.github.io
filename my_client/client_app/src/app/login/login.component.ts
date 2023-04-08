1b7ea1c6cc0821b24d5765dd3ac5783aa29bd216
ngochien19 <ngochien191002@gmail.com>
2023-04-08 23:52:24 +0700

    cài mongoose

--- my_client/client_app/src/app/login/login.component.ts
+++ my_client/client_app/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { SessionStorageService } from '../services/session-storage.service';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -8,21 +9,40 @@ import { SessionStorageService } from '../services/session-storage.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  email: string ='';
-  password: string ='';
-  error: string ='';
+  customerEmail = '';
+  password = '';
+  errorMessage = '';
 
-  constructor(private router: Router, private storageService: SessionStorageService) {}
+  constructor(private loginService: LoginService) { }
 
-  login() {
-    const users = this.storageService.getUsers();
-    const user = users.find(u => u.email === this.email && u.password === this.password);
-    if (user) {
-      this.storageService.setCurrentUser(user);
-      this.router.navigate(['/home']);
-    } else {
-      this.error = 'Invalid email or password';
-    }
+  onSubmit() {
+    this.loginService.postLogin(this.customerEmail, this.password).subscribe(
+      (response: any) => {
+        // Nếu đăng nhập thành công, chuyển hướng tới trang chủ
+        window.location.href = '/home';
+      },
+      (error) => {
+        // Nếu đăng nhập không thành công, hiển thị thông báo lỗi
+        this.errorMessage = 'Email hoặc password không chính xác';
+      }
+    );
   }
+  // email: string ='';
+  // password: string ='';
+  // error: string ='';
+
+  // constructor(private router: Router, private storageService: SessionStorageService) {}
+
+  // login() {
+  //   const users = this.storageService.getUsers();
+  //   const user = users.find(u => u.email === this.email && u.password === this.password);
+  //   if (user) {
+  //     this.storageService.setCurrentUser(user);
+  //     this.router.navigate(['/home']);
+  //   } else {
+  //     this.error = 'Invalid email or password';
+  //   }
+  // }
+
 }
 
