182c67ec840e874f610855699c7c30a57841e319
ngochien19 <ngochien191002@gmail.com>
2023-04-22 00:10:08 +0700

    Hiền update

--- my_client/client_app/src/app/services/storage.service.ts
+++ my_client/client_app/src/app/services/storage.service.ts
@@ -5,31 +5,5 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class StorageService {
-  private identifier =""
-  storageUpdated = new Subject<void>();
 
-  constructor() { }
-
-  setSourceIdentifier(id: string): void {
-    this.identifier = id;
-
-    window.addEventListener('storage', (event: StorageEvent) => {
-      if (event.key === this.identifier) {
-        this.storageUpdated.next();
-      }
-    });
-  }
-
-  save(data: any): void {
-    localStorage.setItem(this.identifier, JSON.stringify(data));
-  }
-
-  fetch(): any {
-    const data = localStorage.getItem(this.identifier) || '';
-    return JSON.parse(data);
-  }
-
-  remove(): void {
-    localStorage.removeItem(this.identifier);
-  }
 }
