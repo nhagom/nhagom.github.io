59aee1e2bcc96a030056ea743f624753d22c88ad
ngochien19 <ngochien191002@gmail.com>
2023-05-10 23:34:52 +0700

    hiền update popup

--- my_client/client_app/src/app/login-page/login/login.component.ts
+++ my_client/client_app/src/app/login-page/login/login.component.ts
@@ -40,8 +40,7 @@ export class LoginComponent implements OnInit{
           });
           this.router.navigate(['/home']);
         } else {
-          console.log("error");
-        }
+          $('#myModal').modal('show');         }
       }
     });
     this.authService.login();
